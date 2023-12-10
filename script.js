5e3dabe92e624110bf37b0b7043938f052df52e3
Leonardo Luz <leonardoluz10@hotmail.com>
2023-12-10 16:10:27 -0300

    Add: classList e forEach para button.

--- script.js
+++ script.js
@@ -4,20 +4,27 @@ const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
 
 focoBtn.addEventListener('click', () => {
     alterarContexto('foco');
+    focoBtn.classList.add('active');
 })
 
 curtoBtn.addEventListener('click', () => {
     alterarContexto('descanso-curto');
+    curtoBtn.classList.add('active');
 })
 
 longoBtn.addEventListener('click', () => {
     alterarContexto('descanso-longo');
+    longoBtn.classList.add('active');
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active');
+    })
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `./imagens/${contexto}.png`);
     switch (contexto) {
