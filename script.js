7f452b9e1d81d8f4d0aaa09710bc2f3cf0659926
Leonardo Luz <leonardoluz10@hotmail.com>
2023-12-11 20:32:34 -0300

    feat: :white_check_mark: Criando função startPauseBt para iniciar contagem.

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('./sons/luna-rise-part-one.mp3');
+const startPauseBt = document.querySelector('#start-pause');
 
 let tempoDecorridoEmSegundos = 5;
-
+let intervaloId = null;
 musica.loop = true;
 
 
@@ -65,4 +66,29 @@ function alterarContexto(contexto) {
         default:
             break;
     }
+}
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        zerar();
+        alert('Tempo finalizado');
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    console.log('temporizador: ' + tempoDecorridoEmSegundos);
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar() {
+    if (intervaloId) {
+        zerar();
+        return
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function zerar(){
+    clearInterval(intervaloId)
+    intervaloId = null
 }
